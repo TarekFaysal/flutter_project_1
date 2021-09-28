b6200b0c56a65b707fec3726ff473800b8651274
Tarek Faysal <faysal5467@gmail.com>
2021-09-28 16:19:46 +0600

    row column text button

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -6,63 +6,31 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.agriculture),
-          onPressed: () {},
-        ),
-        backgroundColor: Colors.green,
-        title: Text("Our first project"),
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                  color: Colors.red,
-                  margin: EdgeInsets.all(20),
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: Text("Hello")),
-              Container(
-                  color: Colors.blue,
-                  margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(5),
-                  child:
-                      ElevatedButton(onPressed: () {}, child: Text("Barisal")))
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                  color: Colors.red,
-                  margin: EdgeInsets.all(20),
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: Text("Hello")),
-              Container(
-                  color: Colors.blue,
-                  margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(5),
-                  child:
-                      ElevatedButton(onPressed: () {}, child: Text("Barisal")))
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                  color: Colors.red,
-                  margin: EdgeInsets.all(20),
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: Text("Hello")),
-              Container(
-                  color: Colors.blue,
-                  margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(5),
-                  child:
-                      ElevatedButton(onPressed: () {}, child: Text("Barisal")))
-            ],
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Text("Hello world"),
+                ElevatedButton(onPressed: null, child: Text("Hello")),
+                Text("Hi")
+              ],
+            ),
+            Row(
+              children: [
+                Text("Hello world"),
+                ElevatedButton(onPressed: null, child: Text("Hello")),
+                Text("Hi")
+              ],
+            ),
+            Row(
+              children: [
+                Text("Hello world"),
+                ElevatedButton(onPressed: null, child: Text("Hello")),
+                Text("Hi")
+              ],
+            )
+          ],
+        ));
   }
 }
