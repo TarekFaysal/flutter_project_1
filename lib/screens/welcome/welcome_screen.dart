9ad528a4221bfeac9bacf02a6b400f121f586d44
Tarek Faysal <faysal5467@gmail.com>
2021-09-29 12:33:57 +0600

    Stack Container AppBar

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -6,31 +6,57 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Text("Hello world"),
-                ElevatedButton(onPressed: null, child: Text("Hello")),
-                Text("Hi")
-              ],
-            ),
-            Row(
-              children: [
-                Text("Hello world"),
-                ElevatedButton(onPressed: null, child: Text("Hello")),
-                Text("Hi")
-              ],
-            ),
-            Row(
-              children: [
-                Text("Hello world"),
-                ElevatedButton(onPressed: null, child: Text("Hello")),
-                Text("Hi")
-              ],
-            )
+        appBar: AppBar(
+          centerTitle: true,
+          title:
+              Text("Dhaka, Bangladesh", style: TextStyle(color: Colors.green)),
+          actions: [
+            IconButton(
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+                onPressed: null),
           ],
+          leading: IconButton(
+            onPressed: null,
+            icon: Icon(Icons.arrow_back, color: Colors.blue),
+          ),
+        ),
+        body: Container(
+          width: 350,
+          height: 350,
+          color: Colors.yellow,
+          child: Column(children: [
+            Row(children: [
+              Container(
+                margin: EdgeInsets.all(20),
+                color: Colors.green,
+                padding: EdgeInsets.all(30),
+                child: Text("Dhaka"),
+              ),
+              Container(
+                margin: EdgeInsets.all(20),
+                color: Colors.red,
+                padding: EdgeInsets.all(30),
+                child: Text("Barisal"),
+              ),
+            ]),
+            Row(children: [
+              Container(
+                margin: EdgeInsets.all(20),
+                color: Colors.red,
+                padding: EdgeInsets.all(30),
+                child: Text("Sylhet"),
+              ),
+              Container(
+                margin: EdgeInsets.all(20),
+                color: Colors.blue,
+                padding: EdgeInsets.all(30),
+                child: Text("Mymensingh"),
+              ),
+            ]),
+          ]),
         ));
   }
 }
